4c7fad3a0fa09576dcfd3848a3f2ac82e5ed2b6e
gyugle <gyugle.9x@gmail.com>
2021-12-16 16:48:53 +0900

    Add css file and apply Detail component.

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -1,20 +1,57 @@
 import { Link } from 'react-router-dom';
-
-function Detail({ bgImg, poster, title, release, rating, runtime, overview }) {
+import styles from '../css/detail.module.css';
+function Detail({
+  bgImg,
+  poster,
+  title,
+  release,
+  genres,
+  rating,
+  runtime,
+  overview,
+}) {
   return (
     <div>
-      <img src={`https://image.tmdb.org/t/p/w300${bgImg}`} alt={title} />
-      <img src={`https://image.tmdb.org/t/p/w300${poster}`} alt={title} />
-      <button>
-        <Link to="/movies">BACK</Link>
-      </button>
-      <h3>{title}</h3>
-      <div>Release Date : {release}</div>
-      <div>Rating : {rating}</div>
-      <div>Runtime : {runtime} mins</div>
-      <div>genres list... why is there error....</div>
-
-      <p>{overview}</p>
+      <div className={styles.background}>
+        <img
+          src={`https://image.tmdb.org/t/p/w1280${bgImg}`}
+          alt={title}
+          className={styles.bgImg}
+        />
+      </div>
+      <Link to="/movies">
+        <button className={styles.btn}>
+          <img
+            src={require('../img/backBtn.png').default}
+            alt="backBtn"
+            className={styles.backBtn}
+          />
+        </button>
+      </Link>
+      <div className={styles.detail}>
+        <div className={styles.poster}>
+          <img src={`https://image.tmdb.org/t/p/w300${poster}`} alt={title} />
+        </div>
+        <div className={styles.description}>
+          <h1>{title}</h1>
+          <hr width={title.length * 18} />
+          <div>Release Date : {release}</div>
+          <div>
+            Genres
+            <ul>
+              {genres.map((genre) => {
+                return <li key={genre.id}> {genre.name} </li>;
+              })}
+            </ul>
+          </div>
+          <div>Rating : {rating}</div>
+          <div>Runtime : {runtime} mins</div>
+          <hr width="50%" />
+          <div>
+            Overview<p className={styles.overview}>{overview}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
